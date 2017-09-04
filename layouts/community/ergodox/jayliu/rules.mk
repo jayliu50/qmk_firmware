16fd959c3aa347ff657aec913c5695e6ef7f5f05
Jay Liu <jayliu50@gmail.com>
2017-09-04 01:38:34 -0400

    Port in my keymap to the new directory structure.

---
+++ layouts/community/ergodox/jayliu/rules.mk
@@ -0,0 +1,7 @@
+SLEEP_LED_ENABLE = no
+DEBUG_ENABLE     = no
+CONSOLE_ENABLE   = no
+TAP_DANCE_ENABLE = yes
+MOUSEKEY_ENABLE  = yes
+NKRO_ENABLE      = yes
+EXTRAKEY_ENABLE  = yes          # Audio control and System control
